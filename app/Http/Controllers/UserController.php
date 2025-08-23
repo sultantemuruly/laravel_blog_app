f9a396c662adc967cc682a6f49fb70b836c6fa72
sultantemuruly <sultantemuruly@gmail.com>
2025-08-23 17:31:04 +0500

    feat: add mysql db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
+        $incomingData = $request->validate([
+            'name' => ['required', 'min:3', 'max:30'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:30']
+        ]);
+        
+        $incomingData['password'] = bcrypt($incomingData['password']);
+        User::create($incomingData);
+
         return "thank you for registering!";
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
         <h2>Register</h2>
         <form action="/register" method="POST">
             @csrf
-            <input type="text" placeholder="Name" /><br /><br />
-            <input type="email" placeholder="Email" /><br /><br />
-            <input type="password" placeholder="Password" /><br /><br />
+            <input name="name" type="text" placeholder="Name" /><br /><br />
+            <input name="email" type="email" placeholder="Email" /><br /><br />
+            <input name="password" type="password" placeholder="Password" /><br /><br />
             <button type="submit">Register</button> 
         </form>
     </div>
