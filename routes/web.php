7f2cbecda7938ca338feb716941fef187299a0f8
sultantemuruly <sultantemuruly@gmail.com>
2025-08-23 16:29:06 +0500

    feat: add UserController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function () {
-    return "thank you for registering!";
-});
+Route::post('/register', [UserController::class, 'register']);
